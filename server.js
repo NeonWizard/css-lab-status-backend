b0a408412b381c756114dd639b52a9426b150acc
Wes <averagewizard13@gmail.com>
2021-11-25 12:30:27 -0800

    Add dotenv

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require('./index')
+require('dotenv').config()
 
-app.listen(3000, (err) => {
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, (err) => {
   if (err) throw err;
-  console.log('Server running on http://127.0.0.1:3000')
+  console.log('Server running on http://127.0.0.1:'+PORT)
 })
